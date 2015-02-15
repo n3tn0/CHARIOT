80c35882063c435879866db5f70ac0fcd52e0bda
n3tn0 <tnoto3@gmail.com>
2015-02-14 21:22:45 -0500

    update opprimo

--- parse.py
+++ parse.py
@@ -6,13 +6,12 @@ import os
 output = open('output.txt', 'r')
 text = output.read()
 
-beginning, discard1 = text.split('...')
-print beginning
-print discard1
+beginning, discard1 = text.split('...')1
 
 discard2,command = beginning.split('\n')
 output.close()
 module = command[0:command.find(' ')]
 folder = module.lower()
 finalcmd = 'python modules/' + folder + '/main.py' + command[command.find(' '):-1]
+print finalcmd
 os.system(finalcmd)
